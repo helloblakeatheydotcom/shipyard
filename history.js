8fe0bda719b66bbffa3fbcfb3141eacfce40ebea
helloblakeatheydotcom <helloblake@hey.com>
2026-01-21 21:10:04 -0500

    Created js revision history file and updated pages to pull revision history from this central file.

---
+++ history.js
@@ -0,0 +1,56 @@
+window.projectHistory = {
+    "add-school": [
+        {
+            "date": "12/02/2025",
+            "ticket": "SHIP-3122",
+            "note": "Initial design of page for Admin to add a new school to the system."
+        }
+    ],
+    "edit-school": [
+        {
+            "date": "12/02/2025",
+            "ticket": "SHIP-3122",
+            "note": "Initial deisng of page for Admin to edit existing schools."
+        }
+    ],
+    "admin-schools": [
+        {
+            "date": "12/02/2025",
+            "ticket": "SHIP-3122",
+            "note": "Initial design of page for Admin to manage all schools.  User can search and sort the table, can view users at the school, inactivate/reactivate the school, add new schools, and edit existing school information."
+        }
+    ],
+    "finances-invoices": [
+        {
+            "date": "01/21/2026",
+            "ticket": "SHIP-3124",
+            "note": "Add filter chips for flagging invoices that need to be reviewed and invoices that are ready to print."
+        },
+        {
+            "date": "01/14/2026",
+            "ticket": "SHIP-3123",
+            "note": "Updated invoice search to include searching on invoice numbers."
+        }
+    ],
+    "invoice-details": [
+        {
+            "date": "Today",
+            "ticket": "SHIP-3121",
+            "note": "Added menu item \"Print & Mark Processed\" to Select Action menu, and the printer-check icon to the status field once the invoice has been processed."
+        }
+    ],
+    "invoice-pdf": [
+        {
+            "date": "01/20/2026",
+            "ticket": "SHIP-3120",
+            "note": "Added approval stamp that displays date the invoice was approved, and the user's initials that approved it.  Displays in format \"Pay MM/DD/YYYY [Initials]\""
+        }
+    ],
+    "ps-applications": [
+        {
+            "date": "01/19/2026",
+            "ticket": "SHIP-3119",
+            "note": "Updated table to display Approved Date column and remove the Initial Year column."
+        }
+    ]
+};
